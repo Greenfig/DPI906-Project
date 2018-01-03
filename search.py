2f47702b40ef87b65889856c6115507231d2f40f
greenfig <rene_anderson@hotmail.com>
2018-01-02 19:43:49 -0500

    updates

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ import requests
 import http.client
 import svgwrite
 import json
+import time
 from googleapiclient.discovery import build
 
 
@@ -12,9 +13,9 @@ def main():
     # the Google APIs Console <http://code.google.com/apis/console>
     # to get an API key for your own application.
     service = build("customsearch", "v1",
-                    developerKey="AIzaSyAwKpKag5OLGW8NSCl2FLgN0LUK7qLoelM")
+                    developerKey="AIzaSyAUR--KI1iW_Ow-PumDDA4rlMrIH8bDLyw")
     res = service.cse().list(
-        q='nordea sweden bank account number',
+        q='salary slip format in excel with formula free downloads',
         cx='000891665926514897966:jvvhkj-zmoe',
     ).execute()
     return res['items']
@@ -32,7 +33,7 @@ def virusTotal(url):
     json_response = response.json()
     dumps = json.dumps(json_response)
     loads = json.loads(dumps)
-    return loads['scans']
+    return loads.get('scans')
 
 
 if __name__ == '__main__':
@@ -46,44 +47,67 @@ if __name__ == '__main__':
            'Connection': 'keep-alive'}
     counter = 0
     for result in results:
-        title = result['title']
-        link = result['link']
-        dis = result['snippet']
-        request = urllib.request.Request(link, headers=hdr)
-        opener = urllib.request.build_opener()
-        f = opener.open(request)
-        print("TITLE : " + title)
-        print("LINK : " + link)
-        print("SNIPPET : " + dis)
-        print("LANDING PAGE : " + f.url)
-        print("=================================================")
-        vtotal = virusTotal(f.url)        
-        dwg = svgwrite.Drawing("test"+str(counter)+".svg", profile='tiny')
-        dwg.add(dwg.circle((0, 0), r=250, fill='blue'))
-        dwg.add(dwg.text(link, insert=(0, 0),
-                text_anchor="middle", fill='red', font_size="10"))
-        dwg.add(dwg.line((250, 0), (650, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
-        dwg.add(dwg.circle((250 + 400 + 250, 0), r=250, fill='blue'))
-        dwg.add(dwg.text(f.url, insert=(250 + 400 + 250, 0),
-                text_anchor="middle", fill='red', font_size="10"))
-        dwg.add(dwg.line((250 + 400 + 250, 250), (250 + 400 + 250, 650),
-                         stroke=svgwrite.rgb(10, 10, 16, '%')))
-        rect_sx = 400
-        rect_sy = 1030
-        dwg.add(dwg.rect(((250 + 400 + 250) - (rect_sx / 2), 650),
-                (rect_sx, rect_sy), fill='grey'))
-        scan_text_start_x = 900
-        scan_text_start_y = 680
-        for scan in vtotal.items():
-          scan_name = scan[0]
-          secondary_items = list(scan[1].items())
-          scan_detected = secondary_items[0][1]
-          scan_result = secondary_items[1][1]
-          if scan_detected:
-            dwg.add(dwg.text(scan_name+" - True - "+scan_result, insert=(scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='red'))
-          else:
-            dwg.add(dwg.text(scan_name+" - False - "+scan_result, insert=(scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='green'))
-          scan_text_start_y += 15
+        response = requests.get(result['link'])
+        dwg = svgwrite.Drawing(
+            "test" + str(counter) + ".svg", profile='tiny')
+        circle_radius = 250
+        circle_lx = 0
+        circle_ly = 0
+        text_x = 0
+        text_y = 0
+        line_x_start = 250
+        line_x_end = 650
+        line_y_start = 0
+        line_y_end = 0
+        if response.history:
+            for redirect in response.history:
+                dwg.add(dwg.circle((circle_lx, circle_ly),
+                        r=circle_radius, stroke='blue', fill='white'))
+                dwg.add(dwg.text(redirect, insert=(text_x, text_y),
+                                          text_anchor="middle", fill='red', font_size="10"))
+                if redirect == response.history[-1]:
+                    line_x_start = line_x_end = circle_radius + \
+                        circle_radius + (line_x_end - line_x_start)
+                    line_y_start = circle_lx + circle_radius
+                    line_y_end = line_y_start + 400
+                    circle_lx = circle_radius + circle_radius + \
+                        (line_x_end - line_x_start)
+                else:
+                    line_x_start = circle_lx + circle_radius
+                    line_x_end = line_x_start + 400
+                dwg.add(dwg.line((line_x_start, line_y_start), (line_x_end, line_y_end), stroke=svgwrite.rgb(10, 10, 16, '%')))
+        else:
+            dwg.add(dwg.circle((circle_lx, circle_ly),
+                    r=circle_radius, stroke='blue', fill='white'))
+            dwg.add(dwg.text(result['link'], insert=(text_x, text_y),
+                                      text_anchor="middle", fill='red', font_size="10"))
+        request=urllib.request.Request(result['link'], headers=hdr)
+        opener=urllib.request.build_opener()
+        f=opener.open(request)
+        vtotal=virusTotal(f.url)
+        if vtotal:
+            rect_sx=400
+            rect_sy=1030
+            rect_lx=line_y_end - (rect_sx / 2)
+            rect_ly=line_y_end
+            dwg.add(dwg.rect((rect_lx, rect_ly),
+                             (rect_sx, rect_sy), fill='grey'))
+            scan_text_start_x=900
+            scan_text_start_y=680
+            for scan in vtotal.items():
+                scan_name=scan[0]
+                secondary_items=list(scan[1].items())
+                scan_detected=secondary_items[0][1]
+                scan_result=secondary_items[1][1]
+                if scan_detected:
+                    dwg.add(dwg.text(scan_name + " - True - " + scan_result, insert=(
+                        scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='red'))
+                else:
+                    dwg.add(dwg.text(scan_name + " - False - " + scan_result, insert=(
+                        scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='green'))
+                scan_text_start_y += 15
+        counter += 1
         dwg.save()
         f.close()
-        counter+=1
+        if counter % 4 == 0:
+            time.sleep(62)
