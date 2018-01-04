789f6170045e494285c8481b8ad957a0a53b7759
greenfig <rene_anderson@hotmail.com>
2018-01-03 22:49:11 -0500

    new updates

--- search.py
+++ search.py
@@ -1,11 +1,20 @@
 import pprint
-import urllib.request
+from urllib.request import urlopen, quote
 import requests
 import http.client
 import svgwrite
 import json
 import time
+from urllib.parse import urljoin
 from googleapiclient.discovery import build
+from bs4 import BeautifulSoup
+import re
+
+class virusTotalCounter:
+    count = 0
+    def value(self):
+        self.count += 1
+        return self.count
 
 
 def main():
@@ -20,8 +29,11 @@ def main():
     ).execute()
     return res['items']
 
-
-def virusTotal(url):
+# Acts as a global variable to control virustotal.com query limit (4 per 60s)
+def virusTotalUrl(url,counter):
+    c = counter.value()
+    if c % 5 == 0:
+        time.sleep(62)
     headers = {
         "Accept-Encoding": "gzip, deflate",
         "User-Agent": "gzip, vtotal"
@@ -35,7 +47,22 @@ def virusTotal(url):
     loads = json.loads(dumps)
     return loads.get('scans')
 
-
+def getDownloadList(url):
+    c = 0
+    u = urlopen(url)
+    try:
+        html = u.read().decode('utf-8')
+    finally:
+        u.close()
+    soup = BeautifulSoup(html)
+    mylist = []
+    for link in soup.find_all('a', href=True):
+        l = link.get('href')
+        regex = re.compile('(\/[a-zA-z0-9-_]+\.[a-zA-Z]+)$')
+        if regex.search(l):
+            mylist.insert(c,l)
+            c += 1
+    return mylist
 if __name__ == '__main__':
     results = main()
     http.client.HTTPConnection.debuglevel = 1
@@ -45,11 +72,13 @@ if __name__ == '__main__':
            'Accept-Encoding': 'none',
            'Accept-Language': 'en-US,en;q=0.8',
            'Connection': 'keep-alive'}
-    counter = 0
+    vt_limit_counter = virusTotalCounter()
     for result in results:
         response = requests.get(result['link'])
+        svgfile_regex = re.compile('(\/\/[a-zA-z0-9-_]+\.[a-zA-Z]+)')
+        svgfile_name = svgfile_regex.search(result['link']).group(1).replace("//","").replace(".com","")
         dwg = svgwrite.Drawing(
-            "test" + str(counter) + ".svg", profile='tiny')
+            svgfile_name + ".svg", profile='tiny')
         circle_radius = 250
         circle_lx = 0
         circle_ly = 0
@@ -59,55 +88,88 @@ if __name__ == '__main__':
         line_x_end = 650
         line_y_start = 0
         line_y_end = 0
+        # Follow redirects
         if response.history:
-            for redirect in response.history:
+            for redirect in response.history:                
                 dwg.add(dwg.circle((circle_lx, circle_ly),
-                        r=circle_radius, stroke='blue', fill='white'))
-                dwg.add(dwg.text(redirect, insert=(text_x, text_y),
-                                          text_anchor="middle", fill='red', font_size="10"))
-                if redirect == response.history[-1]:
-                    line_x_start = line_x_end = circle_radius + \
-                        circle_radius + (line_x_end - line_x_start)
-                    line_y_start = circle_lx + circle_radius
-                    line_y_end = line_y_start + 400
-                    circle_lx = circle_radius + circle_radius + \
-                        (line_x_end - line_x_start)
-                else:
+                                   r=circle_radius, stroke='blue', fill='white'))
+                dwg.add(dwg.text(redirect.url, insert=(text_x, text_y),
+                                 text_anchor="middle", fill='red', font_size="10"))
+                # if there are more urls to add calculate the new circle and print the line horizontally          
+                if redirect != response.history[-1]:
+                    # calculate the line position
                     line_x_start = circle_lx + circle_radius
                     line_x_end = line_x_start + 400
-                dwg.add(dwg.line((line_x_start, line_y_start), (line_x_end, line_y_end), stroke=svgwrite.rgb(10, 10, 16, '%')))
+                    dwg.add(dwg.line((line_x_start, line_y_start), (line_x_end, line_y_end), stroke=svgwrite.rgb(10,10,16,'%')))
+                    # calculate new circle coordinates
+                    circle_lx = (line_x_end - line_x_start) + circle_radius
+            landingpage = response.history[-1].url
         else:
             dwg.add(dwg.circle((circle_lx, circle_ly),
-                    r=circle_radius, stroke='blue', fill='white'))
+                               r=circle_radius, stroke='blue', fill='white'))
             dwg.add(dwg.text(result['link'], insert=(text_x, text_y),
-                                      text_anchor="middle", fill='red', font_size="10"))
-        request=urllib.request.Request(result['link'], headers=hdr)
-        opener=urllib.request.build_opener()
-        f=opener.open(request)
-        vtotal=virusTotal(f.url)
+                             text_anchor="middle", fill='red', font_size="10"))      
+            landingpage = response.url
+        # Query virus total on url
+        vtotal = virusTotalUrl(landingpage, vt_limit_counter)
+        line_x_start = line_x_end = circle_lx
+        line_y_start = line_x_start + circle_radius
+        line_y_end = line_y_start + 400
+        dwg.add(dwg.line((line_x_start, line_y_start), (line_x_end,line_y_end), stroke=svgwrite.rgb(10, 10, 16, '%')))
+        rect_sx = 400
+        rect_sy = 30
+        rect_lx = line_x_end - (rect_sx / 2)
+        rect_ly = line_y_end
+        rectangle = dwg.add(dwg.rect((rect_lx, rect_ly),
+                        (rect_sx, rect_sy), fill='grey'))
+        scan_text_start_x = rect_lx + 200
+        scan_text_start_y = rect_ly + 20
+        isMalicious = False
         if vtotal:
-            rect_sx=400
-            rect_sy=1030
-            rect_lx=line_y_end - (rect_sx / 2)
-            rect_ly=line_y_end
-            dwg.add(dwg.rect((rect_lx, rect_ly),
-                             (rect_sx, rect_sy), fill='grey'))
-            scan_text_start_x=900
-            scan_text_start_y=680
-            for scan in vtotal.items():
-                scan_name=scan[0]
-                secondary_items=list(scan[1].items())
-                scan_detected=secondary_items[0][1]
-                scan_result=secondary_items[1][1]
-                if scan_detected:
-                    dwg.add(dwg.text(scan_name + " - True - " + scan_result, insert=(
-                        scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='red'))
+            if len(vtotal.keys()) > 0:
+                for scan in vtotal.items():
+                    scan_name = scan[0]
+                    secondary_items = list(scan[1].items())
+                    scan_detected = secondary_items[0][1]
+                    scan_result = secondary_items[1][1]
+                    if scan_detected:
+                        isMalicious = True
+                        dwg.add(dwg.text(scan_name + " - True - " + scan_result, insert=(
+                            scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='red'))
+                        scan_text_start_y += 15
+                        rect_sy += 20
+                        rectangle.set_parameter(y=rect_sy)
+            if not isMalicious:
+                dwg.add(dwg.text("NO MALICIOUS CONTENT FOUND", insert=(
+                    scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='green'))        
+        else:
+            dwg.add(dwg.text("NO SCAN AVAILABLE", insert=(
+                scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='green'))
+        # Get list of downloadable items
+        downloadlist = getDownloadList(landingpage)
+        if len(downloadlist) > 0:
+            
+            for filelink in downloadlist:
+                vtotal = virusTotalUrl(filelink, vt_limit_counter)
+                if vtotal:
+                    if len(vtotal.keys()) > 0:
+                        for scan in vtotal.items():
+                            scan_name = scan[0]
+                            secondary_items = list(scan[1].items())
+                            scan_detected = secondary_items[0][1]
+                            scan_result = secondary_items[1][1]
+                            if scan_detected:
+                                isMalicious = True
+                                dwg.add(dwg.text(scan_name + " - True - " + scan_result, insert=(
+                                    scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='red'))
+                                scan_text_start_y += 15
+                                rect_sy += 20
+                                rectangle.set_parameter(y=rect_sy)
+                    if not isMalicious:
+                        dwg.add(dwg.text("NO MALICIOUS CONTENT FOUND", insert=(
+                            scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='green'))        
                 else:
-                    dwg.add(dwg.text(scan_name + " - False - " + scan_result, insert=(
+                    dwg.add(dwg.text("NO SCAN AVAILABLE", insert=(
                         scan_text_start_x, scan_text_start_y), text_anchor="middle", fill='green'))
-                scan_text_start_y += 15
-        counter += 1
         dwg.save()
-        f.close()
-        if counter % 4 == 0:
-            time.sleep(62)
+        response.close()
